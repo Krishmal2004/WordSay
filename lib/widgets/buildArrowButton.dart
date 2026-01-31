51c02c3780192d5007df57ef99799b3e44ded245
krishmal2004 <krishmaldinidu5466@gmail.com>
2026-02-01 00:19:43 +0530

    update the Arrow button

--- lib/widgets/buildArrowButton.dart
+++ lib/widgets/buildArrowButton.dart
@@ -2,15 +2,23 @@ import "package:flutter/material.dart";
 
 Widget buildArrowButton() {
   return Container(
-    padding: const EdgeInsets.all(20),
+    padding: const EdgeInsets.all(5),
     decoration: BoxDecoration(
       color: Color(0xFF1F1F1F),
       shape: BoxShape.circle,    
     ),
-    child: Icon(
+    /*child: Icon(
       Icons.arrow_outward,
       color: Colors.white,
       size: 20,
+    ),*/
+    child: IconButton(
+      onPressed: () {},
+      icon: Icon(
+        Icons.arrow_outward,
+        color: Colors.white,
+        size: 20,
+      ),
     ),
   );
 
